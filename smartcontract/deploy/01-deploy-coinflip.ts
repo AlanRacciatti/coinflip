8044cc1ce169eba6e7356609d5442453d5457059
AlanRacciatti <alanracciatti1220@gmail.com>
2022-06-23 02:22:29 -0300

    feat: add log with script for verifying contract in etherscan after deploy

--- smartcontract/deploy/01-deploy-coinflip.ts
+++ smartcontract/deploy/01-deploy-coinflip.ts
@@ -1,17 +1,18 @@
 import { Address } from "hardhat-deploy/dist/types";
 import { VRFCoordinatorV2Mock } from "../typechain";
-import { DeployFunction } from "hardhat-deploy/types";
+import { DeployFunction, Deployment } from "hardhat-deploy/types";
 
 import { network } from "hardhat";
 import { developmentChains, networkConfig } from "../helper-hardhat-config";
 import { ContractReceipt, ContractTransaction, ethers } from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import "hardhat-deploy";
 
 const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("2");
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre;
-  const { deploy } = deployments;
+  const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId: number = network.config.chainId!;
   let vrfCoordinatorV2Address: Address | undefined, subscriptionId: any;
@@ -53,6 +54,18 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     args,
     log: true,
   });
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    const deployedContract: Deployment = await deployments.get("Coinflip");
+    log("--------------------------------------");
+    log(
+      `Verify the contract: npx hardhat verify --network ${network.name} ${deployedContract.address} ${args[0]} "${args[1]}" "${args[2]}"`
+    );
+    log("--------------------------------------");
+  }
 };
 
 module.exports = func;
